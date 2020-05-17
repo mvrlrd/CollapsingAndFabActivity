aa61d9d7fb8e48c45cf5631efb683dd4fc1be721
mvrlrd <igrbelykh@gmail.com>
2020-05-17 18:16:13 +0300

    toolbar collapsed!

--- app/src/main/java/ru/mvrlrd/collapsingandfabactivity/MainActivity.java
+++ app/src/main/java/ru/mvrlrd/collapsingandfabactivity/MainActivity.java
@@ -1,14 +1,27 @@
 package ru.mvrlrd.collapsingandfabactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_scrolling);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
